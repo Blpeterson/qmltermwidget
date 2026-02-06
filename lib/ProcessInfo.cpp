686dff83d8f7d647f8bcc1315e3cfc6ca78f6dbf
Alex <alexffabri@gmail.com>
2026-02-06 19:05:34 +0900

    Fix macOS readProcInfo to enable currentDir and foregroundProcessName
    The macOS MacProcessInfo::readProcInfo() was completely stubbed out,
    always returning false. This prevented readCurrentDir() from ever
    being called and made isValid() always fail, so currentDir() and
    foregroundProcessName() returned empty strings on macOS.
    
    Replace the dead code with a working implementation using proc_name()
    from libproc.h (already included) to get the process name, and
    setPid() to satisfy the isValid() check.

--- lib/ProcessInfo.cpp
+++ lib/ProcessInfo.cpp
@@ -863,60 +863,12 @@ public:
 
 private:
     virtual bool readProcInfo(int aPid) {
-        int managementInfoBase[4];
-        size_t mibLength;
-        struct kinfo_proc* kInfoProc;
-/*
-        KDE_struct_stat statInfo;
-
-        // Find the tty device of 'pid' (Example: /dev/ttys001)
-        managementInfoBase[0] = CTL_KERN;
-        managementInfoBase[1] = KERN_PROC;
-        managementInfoBase[2] = KERN_PROC_PID;
-        managementInfoBase[3] = aPid;
-
-        if (sysctl(managementInfoBase, 4, NULL, &mibLength, NULL, 0) == -1) {
-            return false;
-        } else {
-            kInfoProc = new struct kinfo_proc [mibLength];
-            if (sysctl(managementInfoBase, 4, kInfoProc, &mibLength, NULL, 0) == -1) {
-                delete [] kInfoProc;
-                return false;
-            } else {
-                const QString deviceNumber = QString(devname(((&kInfoProc->kp_eproc)->e_tdev), S_IFCHR));
-                const QString fullDeviceName =  QString("/dev/") + deviceNumber.rightJustified(3, '0');
-                delete [] kInfoProc;
-
-                const QByteArray deviceName = fullDeviceName.toLatin1();
-                const char* ttyName = deviceName.data();
-
-                if (KDE::stat(ttyName, &statInfo) != 0)
-                    return false;
-
-                // Find all processes attached to ttyName
-                managementInfoBase[0] = CTL_KERN;
-                managementInfoBase[1] = KERN_PROC;
-                managementInfoBase[2] = KERN_PROC_TTY;
-                managementInfoBase[3] = statInfo.st_rdev;
-
-                mibLength = 0;
-                if (sysctl(managementInfoBase, sizeof(managementInfoBase) / sizeof(int), NULL, &mibLength, NULL, 0) == -1)
-                    return false;
-
-                kInfoProc = new struct kinfo_proc [mibLength];
-                if (sysctl(managementInfoBase, sizeof(managementInfoBase) / sizeof(int), kInfoProc, &mibLength, NULL, 0) == -1)
-                    return false;
-
-                // The foreground program is the first one
-                setName(QString(kInfoProc->kp_proc.p_comm));
-
-                delete [] kInfoProc;
-            }
-            setPid(aPid);
+        char name[PROC_PIDPATHINFO_MAXSIZE];
+        if (proc_name(aPid, name, sizeof(name)) > 0) {
+            setName(QString(name));
         }
+        setPid(aPid);
         return true;
-*/
-        return false;
     }
 
     virtual bool readArguments(int aPid) {
